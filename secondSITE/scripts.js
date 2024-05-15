10d9abe25012d86060bb8aa259c8515b1be305ab
THIAGO <thiagohenssuchi@gmail.com>
2024-05-15 16:11:31 -0400

    site TaskDay

--- secondSITE/scripts.js
+++ secondSITE/scripts.js
@@ -5,35 +5,60 @@ const fullList = document.querySelector('.list__task');
 let myList = [];
 
 function addNewTask() {
-    myList.push(input.value);
+    if(input.value == '' || input.value == undefined){
+        window.alert(`Para poder adicionar, digite algo!`);
+        return;
+    }
+    myList.push({
+        tarefa: input.value,
+        concluida: false
+    })
 
     input.value = '';
 
-    tasks()
+    tasks();
 }
 
 function tasks() {
 
     let newLi = '';
 
-    myList.forEach((tarefa, index) => {
+    myList.forEach((item, index) => {
         newLi = newLi + `
-        <li class="task">
-            <img src="./img1/checked.png" alt="confere_na_tarefa" onclick="taskOky()">
-            <p>${tarefa}</p>
+        <li class="task ${item.concluida && "done"}">
+            <img src="./img1/checked.png" alt="confere_na_tarefa" onclick="taskOky(${index})">
+            <p>${item.tarefa}</p>
             <img src="./img1/trash.png" alt="excluir_a_tarefa" onclick="deletItem(${index})">
         </li> 
      
         `
     })
     fullList.innerHTML = newLi;
+
+    localStorage.setItem('list', JSON.stringify(myList));
+}
+
+function taskOky(index) {
+    myList[index].concluida = !myList[index].concluida;
+
+    tasks();
 }
 
 function deletItem(index) {
-    console.log(index)
-}
-function taskOky() {
-    console.log('task performed');
+    myList.splice(index, 1);
+
+    tasks();
 }
 
+function tasksSalved() {
+    const taskLocalStorage = localStorage.getItem('list')
+
+    if (taskLocalStorage) {
+        myList = JSON.parse(taskLocalStorage)
+    }
+
+    tasks();
+}
+
+tasksSalved()
 button.addEventListener('click', addNewTask);
